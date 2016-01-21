6f9fe00e772117e2ca4048cbb5b6a34341b4be93
Alexander <a.starwars.d@gmail.com>
2016-01-21 17:17:17 +0200

    Adds Cross Domain Ajax Request Support

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ if(process.env.NODE_ENVIRONMENT === 'test') {
 if(process.env.NODE_ENVIRONMENT === 'production') {
     port = process.env.PORT || 5000;
 }
+app.use(function(req, res, next) {
+    res.set('Access-Control-Allow-Origin', 'https://notes-app-backend.herokuapp.com/');
+    next();
+});
 require('./controllers/users_controller')(app);
 require('./controllers/sign_in_controller')(app);
 require('./controllers/notes_controller')(app);
